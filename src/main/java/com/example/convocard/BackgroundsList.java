7073b01c28c151df3e0c4b8aa6a02203465a64d5
anas-tawalbeh <abuhamza.anas@gmail.com>
2020-09-20 10:34:53 +0700

    Adding list_item layout and Backgrounds_list layout with necessarily java files

---
+++ src/main/java/com/example/convocard/BackgroundsList.java
@@ -0,0 +1,14 @@
+package com.example.convocard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BackgroundsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backgrounds_list);
+    }
+}
